25ca48f5f89b95083bf6640d15641dba78e03fbb
Aiia22 <64868714+Aiia22@users.noreply.github.com>
2020-06-03 18:31:34 +0100

    JS added current date and time

---
+++ src/app.js
@@ -0,0 +1,27 @@
+//Current date
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayArray = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = dayArray[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+let currentDate = document.querySelector("#currentDate");
+let currentTime = new Date();
+currentDate.innerHTML = formatDate(currentTime);
